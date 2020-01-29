1b8958a6b864e5bb46ebd21a7213910da6759879
sudeep319 <sudeep319@gmail.com>
2020-01-29 14:08:52 +0530

    heroku setup

--- app_hooked.js
+++ app_hooked.js
@@ -1,4 +1,4 @@
 require('babel-register')({
     presets: ['es2015-node6']
     });
-    require('./app.js');
+    require('./server.js');
--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 import mongoose from 'mongoose';
 import quizRoutes from './routes/quiz.routes';
 import * as userControllet from './controllers/user.controller';
 app.use(cors());
 app.use(bodyParser.json());
 //mongodb connection
-mongoose.connect('mongodb://localhost/quizManagement', {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/quizManagement', {
   useNewUrlParser: true, useUnifiedTopology: true
 });
+mongoose.connection.on('connected',()=>{
+  console.log('mongodb connected')
+})
 userControllet.addDefaultUser(function(isuser){
   console.log(isuser)
 });
@@ -26,6 +30,10 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send('<h2 align=center>Page Not Found!</h2>');
 });
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
